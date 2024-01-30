3d54af84caef23dd3b6bdfc45d355e869c3a5dec
Brian <bstanton773@gmail.com>
2024-01-30 09:51:23 -0600

    Initial commit with Navbar setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Container from 'react-bootstrap/Container'
+import Navigation from './components/Navigation';
+
+
+export default function App() {
+    return (
+        <>
+            <Navigation />
+            <Container>
+                <h1>Hello World</h1>
+            </Container>
+        </>
+    )
+}
